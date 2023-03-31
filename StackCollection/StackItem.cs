a9203c393bed5d341ca3108184fb438e2b91982b
KovalYevhenii <kovalyevhenii@gmail.com>
2023-03-31 12:24:26 +0200

    Stack Item added

---
+++ StackCollection/StackItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackCollection
+{
+    public class StackItem
+    {
+        public string? Value { get; set; }
+           
+        public StackItem? Previous { get; set; }
+        public StackItem(string? value, StackItem previous)
+        {
+            Value = value;
+            Previous = previous;
+        }
+    
+    }
+}
